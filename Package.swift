c1be797f0a07e072697e980f3184ad6f8aefcb8b
hsyyim <imhans313@gmail.com>
2025-04-07 23:41:52 +0900

    Add: Core files

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftNetCore",
+    platforms: [.iOS(.v14)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftNetCore",
+            targets: ["SwiftNetCore"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftNetCore"),
+        .testTarget(
+            name: "SwiftNetCoreTests",
+            dependencies: ["SwiftNetCore"]
+        ),
+    ]
+)
